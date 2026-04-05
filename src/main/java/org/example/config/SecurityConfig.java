1176e1ad55401789d7ed1537a286299d573fcb66
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-05 23:32:45 +0530

    feat: implemented JWT prosess

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.example.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.filter.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,32 +11,41 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Testing වලදී CSRF disable කරන්න ඕනේ
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/customers/register").permitAll()
                         .requestMatchers("/api/v1/admins/register").permitAll()
-                        .requestMatchers("/api/v1/customers/all").permitAll()
-                        .requestMatchers("/api/v1/customers/**").permitAll()
-                        .requestMatchers("/api/v1/admins/**").permitAll()
-                        .anyRequest().authenticated() // අනිත් ඒවාට login වෙන්න ඕනේ
-                ).sessionManagement(session -> session
+                        .requestMatchers("/api/v1/customers/**").authenticated()
+                        .requestMatchers("/api/v1/admins/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
