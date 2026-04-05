978c5446225331aa47771c0702a071f462ebac4a
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-05 22:22:50 +0530

    feat: created auth request and response dtos

---
+++ src/main/java/org/example/controller/AuthController.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.model.dto.AuthResponseDTO;
+import org.example.model.dto.LoginRequestDTO;
+import org.example.model.entity.User;
+import org.example.repository.UserRepository;
+import org.example.util.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String token = jwtUtil.generateToken(user.getEmail(), user.getRole());
+
+        return ResponseEntity.ok(new AuthResponseDTO(token, user.getRole(), user.getEmail()));
+    }
+}
