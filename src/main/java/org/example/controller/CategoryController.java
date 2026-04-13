f0f41410d4fbae3c64e0f8f1f7c81f5ba54b7d17
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-13 13:39:37 +0530

    feat: created CategoryController

---
+++ src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package org.example.controller;
+
+import org.example.model.dto.request.LookupDTO;
+import org.example.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@CrossOrigin
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<LookupDTO> getAll() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping
+    public LookupDTO add(@RequestBody String name) {
+        return categoryService.saveCategory(name);
+    }
+}
