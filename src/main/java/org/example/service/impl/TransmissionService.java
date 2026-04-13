76db563bbdde0c80b7f500f2fde147bb828825e7
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-13 16:21:49 +0530

    feat: implemented get all transmissions

---
+++ src/main/java/org/example/service/impl/TransmissionService.java
@@ -0,0 +1,23 @@
+package org.example.service.impl;
+
+import org.example.model.dto.request.LookupDTO;
+import org.example.repository.TransmissionRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TransmissionService implements org.example.service.TransmissionService {
+
+    private TransmissionRepository transmissionRepository;
+    @Override
+    public List<LookupDTO> getAllTransmissions() {
+        return transmissionRepository.findAll().stream()
+                .map(t -> new LookupDTO(t.getId(), t.getTransmissionType()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public LookupDTO saveTransmission(String type) {
+        return null;
+    }
+}
