d78c2e61814c68307d8f684f363fac7c15fed4a1
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-13 13:33:52 +0530

    feat: created category entity

---
+++ src/main/java/org/example/model/entity/Category.java
@@ -0,0 +1,18 @@
+package org.example.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String categoryName;
+}
