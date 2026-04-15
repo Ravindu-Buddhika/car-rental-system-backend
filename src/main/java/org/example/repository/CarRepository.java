b8b32c72dbc005616c7eca2cf5d3634cc9fa97b6
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-15 22:46:38 +0530

    feat: created car Repository

--- src/main/java/org/example/repository/CarRepository.java
+++ src/main/java/org/example/repository/CarRepository.java
@@ -11,11 +11,11 @@ public interface CarRepository extends JpaRepository<CarDetails, Long> {
 
     // Advance Filter Query
     @Query("SELECT c FROM CarDetails c WHERE " +
-            "(:brandId IS NULL OR c.brand.carId = :brandId) AND " +
-            "(:categoryId IS NULL OR c.category.carId = :categoryId) AND " +
-            "(:fuelTypeId IS NULL OR c.fuelType.carId = :fuelTypeId) AND " +
-            "(:transmissionId IS NULL OR c.transmission.carId = :transmissionId) AND " +
-            "(:capacityId IS NULL OR c.seatingCapacity.carId = :capacityId) AND " +
+            "(:brandId IS NULL OR c.brand.id = :brandId) AND " + // මෙතන .id විය යුතුයි
+            "(:categoryId IS NULL OR c.category.id = :categoryId) AND " +
+            "(:fuelTypeId IS NULL OR c.fuelType.id = :fuelTypeId) AND " +
+            "(:transmissionId IS NULL OR c.transmission.id = :transmissionId) AND " +
+            "(:capacityId IS NULL OR c.seatingCapacity.id = :capacityId) AND " +
             "(:status IS NULL OR c.status = :status)")
     List<CarDetails> findCarsByFilters(
             @Param("brandId") Long brandId,
@@ -23,7 +23,8 @@ public interface CarRepository extends JpaRepository<CarDetails, Long> {
             @Param("fuelTypeId") Long fuelTypeId,
             @Param("transmissionId") Long transmissionId,
             @Param("capacityId") Long capacityId,
-            @Param("status") String status);
+            @Param("status") String status
+    );
 
     List<CarDetails> findByCarModelContainingIgnoreCase(String model);
 }
