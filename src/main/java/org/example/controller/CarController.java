97103f9f99d905372245f18957e039f7baf8ffba
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-15 22:45:17 +0530

    feat: updated car controller

--- src/main/java/org/example/controller/CarController.java
+++ src/main/java/org/example/controller/CarController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/cars")
 @RequiredArgsConstructor
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 public class CarController {
 
     private final CarService carService;
